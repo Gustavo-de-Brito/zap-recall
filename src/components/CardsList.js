21ba2faf67f26e585badadc0b1b9953d7c223525
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-05-14 15:42:46 -0300

    adiciona randomizacao de cartas e respostas fixas

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -2,10 +2,16 @@ import React from "react";
 import Card from "./Card";
 
 export default function CardsList({ cardsContent, iconsList, setIconsList }) {
+    function shuffle() {
+        return Math.random() - 0.5;
+    }
+
+    const shuffledCards = cardsContent.sort(shuffle);
+
     return (
         <ul className="cards">
             {
-            cardsContent.map( ( cardContent, index ) => 
+            shuffledCards.map( ( cardContent, index ) => 
                 <Card 
                     key={index}
                     front={ cardContent.front } 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,6 @@
 export default function Question({ questionNum, showQuestion, cardState, iconType }) {
     return (
-        <li className={ `question ${ cardState }` } onClick={ showQuestion }>
+        <li className={ `question ${ cardState }` } onClick={ cardState === "" ? showQuestion : () => "" }>
             <span>Pergunta { questionNum }</span> <ion-icon name={ iconType } ></ion-icon>
         </li>
     )
